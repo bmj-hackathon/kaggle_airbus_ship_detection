705b78c021dec098f34f066a86bbe570e98cc8e6
MarcusJones <jones.0bj3@gmail.com>
2019-07-19 18:56:39 +0200

    update

--- reference_guides/knn_test.py
+++ reference_guides/knn_test.py
@@ -1,14 +1,102 @@
 # import the necessary packages
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from imutils import paths
+
+import imutils
+import importlib
+importlib.reload(imutils)
+
 import numpy as np
 import argparse
 import imutils
 import cv2
 import os
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.image as mplimg
+from matplotlib.pyplot import imshow
+
+from pathlib import Path
+
+imutils.mj_paper
+imutils.text
+
+#%%
+data_path = Path("/media/batman/f4023177-48c1-456b-bff2-cc769f3ac277/ASSETS/Dogs vs Cats")
+image_path = data_path / '12499.jpg'
+image_path = data_path / '12500.jpg'
+assert image_path.exists()
 
 
 #%%
-data_path = "/media/batman/f4023177-48c1-456b-bff2-cc769f3ac277/ASSETS/Dogs vs Cats"
+def open_rgb(image_path):
+    img = cv2.imread(str(image_path))
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#%%
+def extract_color_histogram(image, bins=(8, 8, 8)):
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    hist = cv2.calcHist([hsv], [0, 1, 2], None, bins, [0, 180, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return hist.flatten()
+
+def plot_hist(img):
+    color = ('r', 'g', 'b')
+    for i, col in enumerate(color):
+        histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+        plt.plot(histr, color=col)
+        plt.xlim([0, 256])
+    plt.show()
+
+#%%
+# The image is stored as a np.ndarray
+# Attributes"
+# .shape - x, y, z
+# .min 0
+# .max 255
+#
+img_original = open_rgb(image_path)
+plt.figure("TITLE")
+plt.imshow(img_original)
+plt.show()
+
+plot_hist(img_original)
+# hist = extract_color_histogram(img_original)
+
+#%%
+size=(32, 32)
+img2 = cv2.resize(img_original, size)
+plt.imshow(img2)
+plt.show()
+
+plot_hist(img_original)
+
+#%%
+
+
+fig = plt.figure(figsize=PAPER['A3_LANDSCAPE'], facecolor='white')
+fig.suptitle("Test {}".format('TEst'), fontsize=20)
+
+nrows = 3
+nrowplots = nrows * 2
+ncols = 3
+height_ratios = [3,1] * nrows
+width_ratios = [1] * ncols
+axes = plt.subplots(nrowplots, ncols, gridspec_kw={'width_ratios': width_ratios, 'height_ratios':height_ratios })
+
+
+plt.show()
+
+
+for i, img_path in enumerate(sel_img_paths):
+    logging.info("{}".format(img_path))
+    ax = fig.add_subplot(ROWS, COLS, i + 1)
+    img = mpl.image.imread(img_path)
+    ax.imshow(img)
+    ax.axis('off')
+    # plt.title(str_label)
+plt.show()
+
+#%%
+img2.flatten()
